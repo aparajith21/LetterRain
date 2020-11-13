29f45228757e9b6ca9693261774236eda9cae9d2
aparajith21 <aparajithr21@gmail.com>
2020-11-13 21:13:13 +0530

    added letter generator

---
+++ main.py
@@ -0,0 +1,12 @@
+import GenerateLetters
+def main():
+    n = 5
+    for i in range(10):
+        letters = GenerateLetters.GenerateLetters(n)
+        print(letters)
+
+
+
+if __name__ == "__main__":
+    main()
+
